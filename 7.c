2efd1dc71efe765ab1e9e96b0fe811c1ded025e8
vasan06 <keerthivasan83000@gmail.com>
2026-03-11 01:59:16 +0900

    Add C program to execute and display date command

---
+++ 7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  FILE *fp;
+  char data[100];
+  fp=popen("date","r");
+  if(fp==NULL)
+  {
+    printf("Error opening pipe\n");
+    return 1;
+  }
+while(fgets(data,sizeof(data),fp)!=NULL)
+{
+  printf("Output : %s\n",data)
+}
+pclose(fp);
+return 0;
+}
