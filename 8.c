4400a5bbde67124e1eaf7e55e95edd54c4a2cf9e
vasan06 <keerthivasan83000@gmail.com>
2026-04-07 20:40:55 +0530

    Update 8.c

--- 8.c
+++ 8.c
@@ -21,25 +21,4 @@ int main()
     return 0;
 }
 
-//8b
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 
-int main()
-{
-    int pid = fork();
-
-    if(pid == 0)   
-    {
-        printf("Child exiting\n");
-        exit(0);
-    }
-    else           
-    {
-        sleep(10);   
-        printf("Parent exiting\n");
-    }
-
-    return 0;
-}
